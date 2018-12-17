4f934c467dfd0757d0a94118faa28cb37998abc6
Aditya Mudgil <adityamudgil2505@gmail.com>
2018-12-17 18:33:25 +0530

    Update SideBar, volume, favourite
    All the things are updated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var albumName = ["Imagine Dragon - Evolve","Maroon 5 - V", "Fate of the Furious", "Zero"];
 var albumArt = ["Believer.jpeg", "Sugar.png", "GoodLife.jpg", "Zero.jpg"];
 var title =["Believer", "Sugar", "Good Life", "Mere naam tu"];
-var durationList=[];
+var durationList=["3:37","5:01","3:51","2:00"];
 var sequence=[];
+var favList=[];
 
+var favBtn = $('.unfav i');
 var pauseAndPlay = $(".play i");
 var nextBtn = $(".next i");
 var prevBtn = $(".back i");
@@ -17,7 +19,13 @@ for(var i=0;i<noOfSongs;i++)
 { sequence.push(i);
 }
 
+//intializing favList
+for(var i=0;i<noOfSongs;i++)
+{ favList.push(0);
+}
+
 //Calculating duration of all songs
+/*
 window.onload=loadDuration;
 function loadDuration(){
   for(var i=0 ;i<noOfSongs; i++)
@@ -34,19 +42,27 @@ function loadDuration(){
   }
   playSong();
 }
-
+*/
 var song = new Audio();
 var currentSong=0;
 var cTime=0;
 var repeat=false;
 
 //play song function with totaltime update
+window.onload=playSong;
 function playSong()
 { console.log("Window is updated");
   song.src="songs/"+title[sequence[currentSong]]+'.mp3';
   $(".albumArt img").attr("src","images/"+albumArt[sequence[currentSong]]);
   $(".ablumName").text(albumName[sequence[currentSong]]);
   $(".title").text(title[sequence[currentSong]]);
+  // fav
+  if(favList[sequence[currentSong]])
+  { favBtn.attr('class',"fas fa-heart");
+  }
+  else
+  { favBtn.attr('class',"far fa-heart");
+  }
   let temp=setInterval(function(){
     if(song.readyState>0)
     { $(".totalTime").text(durationList[sequence[currentSong]]);
@@ -172,6 +188,7 @@ volBtn.on('click',function(){
     changeVol();
     volBtn.removeClass('fa-volume-up');
     volBtn.addClass('fa-volume-mute');
+    console.log(song.volume);
   }
 });
 function changeVol()
@@ -181,4 +198,63 @@ function changeVol()
   { volBtn.removeClass('fa-volume-up');
     volBtn.addClass('fa-volume-mute');
   }
+  else{
+    volBtn.removeClass('fa-volume-mute');
+    volBtn.addClass('fa-volume-up');
+  }
 }
+
+//favourite Button
+
+favBtn.on('click',function(){
+  // making fav
+  if(favBtn.attr('class')=="far fa-heart")
+  { favBtn.removeClass('far fa-heart');
+    favBtn.addClass('fas fa-heart');
+    favList[sequence[currentSong]]=1;
+  }
+  // making unfav
+  else{
+    favBtn.removeClass('fas fa-heart');
+    favBtn.addClass('far fa-heart');
+    favList[sequence[currentSong]]=0;
+  }
+});
+
+var sideBarFav = $("#sideBarFav");
+var favouriteSong=$(".side-menu .side-popular-music");
+//sideBar
+$(".header .navi span").click(function(){
+  $(".side-menu").animate({marginLeft: '0px'});
+  $(".volume-slider").animate({marginTop: '0px'}, 500);
+  //updateSideBar();
+  sideBarFav.hide();
+}); 
+$(".side-menu li a, .side-menu .close").click(function(){
+  $(".side-menu").animate({marginLeft: '-310px'});
+}); 
+
+// update favourite song sidebar
+favouriteSong.on('click',function(){
+    $('#sideBarFav').toggle();
+    updateSideBar();
+});
+function updateSideBar()
+{ 
+  $('#sideBarFav .insideEle').remove();
+  for(var i=0;i<noOfSongs;i++)
+  { if(favList[i])
+    { var favSong = "<div class='insideEle' id='"+sequence[i]+"'>"+title[i]+"</div>";
+      sideBarFav.append(favSong);
+    }
+  }
+}
+
+// on click to favourite song
+$(document.body).on('click', '#sideBarFav div' ,function()
+  { console.log("Button is clicked");
+    currentSong = $(this).attr('id');
+    playSong();
+    $(".side-menu").animate({marginLeft: '-310px'},1000);
+  }
+);
